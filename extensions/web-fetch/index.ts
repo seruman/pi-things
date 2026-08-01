afeeab5130e2c4598904ec717aa8eeac8f67c348
selman <selmankayrancioglu@gmail.com>
2026-08-01 22:58:32 +0300

    web-tools: consolidate extension entrypoints

--- extensions/web-fetch/index.ts
+++
@@ -1,8 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent"
-import { registerFetchContentTool } from "../web-tools/fetch/fetch-content.tool"
-import { registerFetchRenderedTool } from "../web-tools/fetch/fetch-rendered.tool"
-
-export default function (pi: ExtensionAPI) {
-	registerFetchContentTool(pi)
-	registerFetchRenderedTool(pi)
-}
--- extensions/web-search/index.ts
+++
@@ -1,6 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent"
-import { registerBraveSearchTool } from "../web-tools/search/brave-search.tool"
-
-export default function (pi: ExtensionAPI) {
-	registerBraveSearchTool(pi)
-}
